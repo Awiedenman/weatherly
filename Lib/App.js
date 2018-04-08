7cc25eddb23e8ad0b2a69e08eea540583b207783
Austin.Wiedenman <awiedenman@gmail.com>
2018-04-08 17:15:57 -0600

    Setup first render to the browser

---
+++ Lib/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../Styles/App.css';
+import '../Data/mock-data.js';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                    <h1> i love the the weather!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
