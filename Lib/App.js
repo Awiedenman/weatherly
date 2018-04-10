d95101e439cee14d1fe9a6e23f44bbebc6b96a41
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-04-09 18:41:55 -0600

    Connect mock data

--- Lib/App.js
+++ Lib/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react'
 import '../Styles/App.css';
-import '../Data/mock-data.js';
+import data from '../Data/mock-data.js';
+import CurrentWeather from './CurrentWeather.js';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {};
-    }
+  constructor() {
+    super();
+      
+  }
 
-    render() {
-        return (
-            <div>
-                    <h1> i love the the weather!</h1>
+  render () {
+    console.log('poop')
+    return (
+            <div>    
+                <h1> i love the the weather!</h1>
+                <CurrentWeather 
+                    weather={data.current_observation.display_location.full}
+                    />      
             </div>
         )
-    }
+  }
 }
 
 export default App;
--- Lib/CurrentWeather.js
+++ Lib/CurrentWeather.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import '../Styles/CurrentWeather.css';
+// import PropTypes from 'prop-types';
+
+
+const CurrentWeather = (props)=>{
+  const {weather, } = props;
+  return (
+      <div>
+        <h2>{weather}</h2>
+      </div>
+    );  
+};
+
+
+export default CurrentWeather;
