a625080e19b8bf2bf9dbe6955129a64411bf4bbe
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-04-15 11:21:39 -0600

    Add modest styling to cards and button and search field

--- Lib/Search.js
+++ Lib/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../Styles/Search.css'
 
 class Search extends Component {
   constructor() {
@@ -19,11 +20,15 @@ class Search extends Component {
 
   render() {
     return (
-      <form>
+      <form role="search">
         <input
-          placeholder="Search for..."
           ref={input => this.search = input}
           onChange={this.userInputChanges}
+          type="search"
+          placeholder="City, St OR zipcode"
+          name="q"
+          size="40"
+          required
         />
         <p>{this.state.query}</p>
       </form>
